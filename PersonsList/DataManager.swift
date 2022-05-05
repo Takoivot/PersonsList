ecb2f48186731fd52b9e442526388c6b4db82169
Artur Sakhbiev <56787456+Takoivot@users.noreply.github.com>
2022-05-05 21:36:49 +0300

    add DataMng and Person model

---
+++ PersonsList/DataManager.swift
@@ -0,0 +1,54 @@
+//
+//  DataManager.swift
+//  PersonsList
+//
+//  Created by Артур Сахбиев on 05.05.2022.
+//
+
+
+struct Data {
+    let names = ["Bob",
+                 "Mary",
+                 "Luis",
+                 "Frank",
+                 "Ramirez",
+                 "Paul",
+                 "Tom",
+                 "Jack",
+                 "Tim",
+                 "Stanly"
+    ]
+    let surnames = ["Laves",
+                    "Faykerton",
+                    "Simons",
+                    "Lachevski",
+                    "Bilwins",
+                    "Fareev",
+                    "Storchins",
+                    "Rook",
+                    "Sliderson",
+                    "Grachevksy"
+    ]
+    let phoneNumbers = ["111-111",
+                        "222-222",
+                        "333-333",
+                        "444-444",
+                        "555-555",
+                        "666-666",
+                        "777-777",
+                        "888-888",
+                        "999-999",
+                        "123-456"
+    ]
+    let emails = ["qqq@mail.ru",
+                  "www@mail.ru",
+                  "fff@mail.ru",
+                  "rrr@mail.ru",
+                  "ttt@mail.ru",
+                  "yyy@mail.ru",
+                  "uuu@mail.ru",
+                  "iii@mail.ru",
+                  "hhh@mail.ru",
+                  "sss@mail.ru"
+    ]
+}
---
+++ PersonsList/Persons.swift
@@ -0,0 +1,14 @@
+//
+//  Persons.swift
+//  PersonsList
+//
+//  Created by Артур Сахбиев on 05.05.2022.
+//
+
+struct Person {
+    let name: String
+    let surname: String
+    let phoneNumber: String
+    let email: String
+    
+}
