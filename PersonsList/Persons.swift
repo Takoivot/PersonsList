ae0d5322052fdb7681a130155daba7c59b43116c
Artur Sakhbiev <56787456+Takoivot@users.noreply.github.com>
2022-05-09 23:56:54 +0300

    Update Persons.swift

--- PersonsList/Persons.swift
+++ PersonsList/Persons.swift
@@ -11,4 +11,32 @@ struct Person {
     let phoneNumber: String
     let email: String
     
+    
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getPersons() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = Data.shared.names.shuffled()
+        let surnames = Data.shared.surnames.shuffled()
+        let phoneNumbers = Data.shared.phoneNumbers.shuffled()
+        let emails = Data.shared.emails.shuffled()
+        
+        let indexEnd = min(names.count, surnames.count, phoneNumbers.count, emails.count)
+        
+        for index in 1..<indexEnd {
+            let person = Person(name: names[index],
+                                surname: surnames[index],
+                                phoneNumber: phoneNumbers[index],
+                                email: emails[index]
+            )
+            persons.append(person)
+        }
+        return persons
+       
+    }
 }
